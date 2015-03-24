a60a1aaa912d8171b79ffb1f686a211952f42ecb
Simarpreet Singh <simar@linux.com>
2015-03-24 15:04:06 -0400

    loader: Add comments
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- loader.s
+++ loader.s
@@ -4,13 +4,13 @@ MAGIC_NUMBER    equ 0x1BADB002
 FLAGS           equ 0x0
 CHECKSUM        equ -MAGIC_NUMBER
 
-section .text:
-align 4
-    dd MAGIC_NUMBER
+section .text:                      ; start of the code section
+align 4                             ; 4 byte aligned, 4x16=64 word length
+    dd MAGIC_NUMBER                 ; old ye dd util, not the one you think.
     dd FLAGS
     dd CHECKSUM
 
 loader:
-    mov eax, 0xCAFEBABE
+    mov eax, 0xCAFEBABE             ; write the actual value into the eax reg
 .loop:
     jmp .loop
