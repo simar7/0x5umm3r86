2f202c90abba5171530fe803a6cdc41b72dc3c58
Simarpreet Singh <simar@linux.com>
2015-03-25 12:24:08 -0400

    stack: add a stack for C
    All it takes is:
        1) Reserve a block of fixed byte size in .bss
        2) Point esp to the end of the stack (stack grows upwards/lower addresses)
    
    .bss is used rather than data as GRUB is aware of .bss \
    .bss is (statically linked + keeps OS blob file size small)
    
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- loader.s
+++ loader.s
@@ -1,14 +1,21 @@
 global loader
 
-MAGIC_NUMBER    equ 0x1BADB002
-FLAGS           equ 0x0
-CHECKSUM        equ -MAGIC_NUMBER
+MAGIC_NUMBER        equ 0x1BADB002
+FLAGS               equ 0x0
+CHECKSUM            equ -MAGIC_NUMBER
+KERNEL_STACK_SIZE   equ 4096
 
 section .text:                      ; start of the code section
 align 4                             ; 4 byte aligned, 4x16=64 word length
     dd MAGIC_NUMBER                 ; old ye dd util, not the one you think.
     dd FLAGS
     dd CHECKSUM
+
+section .bss:
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE                      ; reserve a 4096 block for stack in .bss
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the end of stack.
 
 loader:
     mov eax, 0xCAFEBABE             ; write the actual value into the eax reg
